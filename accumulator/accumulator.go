0e7ef9be167f11dfdc34109cb70eb1eedf53bdc5
Tommy TIAN <xtianae@connect.ust.hk>
2022-02-08 21:13:42 +0800

    iterative membership witness precomputing

--- accumulator/accumulator.go
+++ accumulator/accumulator.go
@@ -31,14 +31,14 @@ func GenRepersentatives(set []string, encodeType EncodeType) []big.Int {
 func AccAndProve(set []string, encodeType EncodeType, setup *AccumulatorSetup) (*big.Int, []big.Int) {
 	rep := GenRepersentatives(set, encodeType)
 
-	proofs := ProveMembership(&setup.G, &setup.N, rep)
+	proofs := ProveMembershipIterative(&setup.G, &setup.N, rep)
 	// we generate the accumulator by anyone of the membership proof raised to its power to save some calculation
 	acc := Accumulate(&proofs[0], &rep[0], &setup.N)
 
 	return acc, proofs
 }
 
-// ProveMembership uses divide-and-conqure method to pre-compute the all membership proofs in time O(nlogn)
+// ProveMembership uses divide-and-conquer method to pre-compute the all membership proofs in time O(nlogn)
 func ProveMembership(base, N *big.Int, set []big.Int) []big.Int {
 	if len(set) == 1 {
 		ret := make([]big.Int, 1)
@@ -56,6 +56,44 @@ func ProveMembership(base, N *big.Int, set []big.Int) []big.Int {
 	rightBase := *accumulate(set[0:len(set)/2], base, N)
 	proofs := ProveMembership(&leftBase, N, set[0:len(set)/2])
 	proofs = append(proofs, ProveMembership(&rightBase, N, set[len(set)/2:])...)
+	return proofs
+}
+
+func ProveMembershipIterative(base, N *big.Int, set []big.Int) []big.Int {
+	var (
+		left           int
+		right          int = len(set)
+		intervals      [][2]int
+		newIntervals   [][2]int
+		intervalLen    int = 1
+		newIntervalLen int
+		proofs         []big.Int
+		newProofs      []big.Int
+	)
+	intervals = append(intervals, [2]int{left, right})
+	proofs = append(proofs, *base)
+	for intervalLen != newIntervalLen {
+		for idx, interval := range intervals {
+			left, right = interval[0], interval[1]
+			if right-left <= 1 {
+				newIntervals = append(newIntervals, [2]int{left, right})
+				newProofs = append(newProofs, proofs[idx])
+				continue
+			}
+			mid := right - (right-left)/2
+			newIntervals = append(newIntervals, [2]int{left, mid})
+			newIntervals = append(newIntervals, [2]int{mid, right})
+			acc := proofs[idx]
+			newProofs = append(newProofs, *accumulate(set[mid:right], &acc, N))
+			newProofs = append(newProofs, *accumulate(set[left:mid], &acc, N))
+		}
+		intervalLen = len(intervals)
+		newIntervalLen = len(newIntervals)
+		intervals = newIntervals
+		newIntervals = nil
+		proofs = newProofs
+		newProofs = nil
+	}
 	return proofs
 }
 
