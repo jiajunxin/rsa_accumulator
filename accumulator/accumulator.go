7a57d24780172c89f7e9f3b6dbb0c81bf6de83e7
tommytim0515 <xtianae@connect.ust.hk>
2022-02-21 14:39:26 +0800

    change ProveMembershipIter

--- accumulator/accumulator.go
+++ accumulator/accumulator.go
@@ -38,7 +38,7 @@ func AccAndProve(set []string, encodeType EncodeType, setup *AccumulatorSetup) (
 	return acc, proofs
 }
 
-// AccAndProveIter ...
+// AccAndProveIter iteratively generates the accumulator with all the memberships precomputed
 func AccAndProveIter(set []string, encodeType EncodeType, setup *AccumulatorSetup) (*big.Int, []big.Int) {
 	rep := GenRepersentatives(set, encodeType)
 
@@ -70,43 +70,68 @@ func ProveMembership(base, N *big.Int, set []big.Int) []big.Int {
 	return proofs
 }
 
-// ProveMembershipIter ....
+// ProofIterator is the struct for iterating
+type proofIterator struct {
+	left  int
+	right int
+	proof big.Int
+	next  *proofIterator
+}
+
+// ProveMembershipIter ...
 func ProveMembershipIter(base, N *big.Int, set []big.Int) []big.Int {
 	var (
-		left         int
-		right        int = len(set)
-		intervals    [][2]int
-		newIntervals [][2]int
-		finishFlag   bool
-		proofs       []big.Int
-		newProofs    []big.Int
+		dummy *proofIterator = &proofIterator{
+			next: &proofIterator{
+				left:  0,
+				right: len(set),
+				proof: *base,
+				next:  nil,
+			},
+		}
+		prev       *proofIterator = dummy
+		iter       *proofIterator = dummy.next
+		finishFlag bool
 	)
-	intervals = append(intervals, [2]int{left, right})
-	proofs = append(proofs, *base)
 	for {
-		for idx, interval := range intervals {
-			left, right = interval[0], interval[1]
+		for iter != nil {
+			left := iter.left
+			right := iter.right
 			if right-left <= 1 {
-				newIntervals = append(newIntervals, [2]int{left, right})
-				newProofs = append(newProofs, proofs[idx])
+				iter = iter.next
+				prev = prev.next
 				continue
 			}
 			mid := right - (right-left)/2
-			newIntervals = append(newIntervals, [2]int{left, mid})
-			newIntervals = append(newIntervals, [2]int{mid, right})
-			acc := proofs[idx]
-			newProofs = append(newProofs, *accumulate(set[mid:right], &acc, N))
-			newProofs = append(newProofs, *accumulate(set[left:mid], &acc, N))
+			acc := iter.proof
+			secondNewProof := &proofIterator{
+				left:  mid,
+				right: right,
+				proof: *accumulate(set[left:mid], &acc, N),
+				next:  iter.next,
+			}
+			firstNewProof := &proofIterator{
+				left:  left,
+				right: mid,
+				proof: *accumulate(set[mid:right], &acc, N),
+				next:  secondNewProof,
+			}
+			prev.next = firstNewProof
+			iter = iter.next
+			prev = secondNewProof
 			finishFlag = true
 		}
-		intervals = newIntervals
-		newIntervals = nil
-		proofs = newProofs
-		newProofs = nil
 		if !finishFlag {
 			break
 		}
 		finishFlag = false
+		prev = dummy
+		iter = dummy.next
+	}
+	proofs := make([]big.Int, len(set))
+	for i := 0; i < len(set); i++ {
+		proofs[i] = dummy.next.proof
+		dummy = dummy.next
 	}
 	return proofs
 }
--- accumulator/accumulator_test.go
+++ accumulator/accumulator_test.go
@@ -107,59 +107,75 @@ func TestAccAndProve(t *testing.T) {
 	}
 }
 
-func TestAccAndProveIter(t *testing.T) {
-	setup := TrustedSetup()
-
-	testSetSize := 16
-	set := GenTestSet(testSetSize)
-	acc, proofs := AccAndProveIter(set, HashToPrimeFromSha256, setup)
-	if len(set) != len(proofs) {
-		t.Errorf("proofs have different size as the input set")
-	}
+func genAccts(set []string, setup *AccumulatorSetup, proofs []big.Int, idx int) (acc1, acc2 *big.Int) {
 	rep := GenRepersentatives(set, HashToPrimeFromSha256)
-	acc2 := accumulate(rep, &setup.G, &setup.N)
-	acc3 := Accumulate(&proofs[5], &rep[5], &setup.N)
-	if acc.Cmp(acc3) != 0 {
-		t.Errorf("proofs generated are not consistent")
-	}
-	if acc2.Cmp(acc3) != 0 {
-		t.Errorf("proofs generated are not consistent")
-	}
+	acc1 = accumulate(rep, &setup.G, &setup.N)
+	acc2 = Accumulate(&proofs[idx], &rep[idx], &setup.N)
+	return
+}
 
-	// test another set size not a power of 2
-	testSetSize = 17
-	set = GenTestSet(testSetSize)
-	acc, proofs = AccAndProveIter(set, HashToPrimeFromSha256, setup)
-	if len(set) != len(proofs) {
-		t.Errorf("proofs have different size as the input set")
-	}
-	rep = GenRepersentatives(set, HashToPrimeFromSha256)
-	acc2 = accumulate(rep, &setup.G, &setup.N)
-	acc3 = Accumulate(&proofs[7], &rep[7], &setup.N)
-	if acc.Cmp(acc3) != 0 {
-		t.Errorf("proofs generated are not consistent")
-	}
-	if acc2.Cmp(acc3) != 0 {
-		t.Errorf("proofs generated are not consistent")
-	}
+func TestAccAndProveIter(t *testing.T) {
 
-	// test another set size not a power of 2
-	testSetSize = 254
-	set = GenTestSet(testSetSize)
-	acc, proofs = AccAndProveIter(set, HashToPrimeFromSha256, setup)
-	if len(set) != len(proofs) {
-		t.Errorf("proofs have different size as the input set")
+	type args struct {
+		set        []string
+		encodeType EncodeType
+		setup      *AccumulatorSetup
 	}
-	rep = GenRepersentatives(set, HashToPrimeFromSha256)
-	acc2 = accumulate(rep, &setup.G, &setup.N)
-	acc3 = Accumulate(&proofs[253], &rep[253], &setup.N)
-	if acc.Cmp(acc3) != 0 {
-		t.Errorf("proofs generated are not consistent")
+	tests := []struct {
+		name         string
+		args         args
+		idx          int
+		wantProofLen int
+	}{
+		{
+			name: "set_size_16",
+			args: args{
+				set:        GenTestSet(16),
+				encodeType: HashToPrimeFromSha256,
+				setup:      TrustedSetup(),
+			},
+			idx:          5,
+			wantProofLen: 16,
+		},
+		{
+			name: "set_size_17",
+			args: args{
+				set:        GenTestSet(17),
+				encodeType: HashToPrimeFromSha256,
+				setup:      TrustedSetup(),
+			},
+			idx:          7,
+			wantProofLen: 17,
+		},
+		{
+			name: "set_size_254",
+			args: args{
+				set:        GenTestSet(254),
+				encodeType: HashToPrimeFromSha256,
+				setup:      TrustedSetup(),
+			},
+			idx:          253,
+			wantProofLen: 254,
+		},
 	}
-	if acc2.Cmp(acc3) != 0 {
-		t.Errorf("proofs generated are not consistent")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			acc, proofs := AccAndProveIter(tt.args.set, tt.args.encodeType, tt.args.setup)
+			acc1, acc2 := genAccts(tt.args.set, tt.args.setup, proofs, tt.idx)
+			if len(proofs) != tt.wantProofLen {
+				t.Errorf("AccAndProveIter() got proof len = %v, want %v", len(proofs), tt.wantProofLen)
+				return
+			}
+			if acc.Cmp(acc2) != 0 {
+				t.Errorf("proofs generated are not consistent acc = %v, acc2 %v", acc, acc2)
+				return
+			}
+			if acc1.Cmp(acc2) != 0 {
+				t.Errorf("proofs generated are not consistent acc1 = %v, acc2 %v", acc1, acc2)
+				return
+			}
+		})
 	}
-
 }
 
 func GenTestSet(num int) []string {
