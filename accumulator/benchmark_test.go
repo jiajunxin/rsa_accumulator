ecc638fad05d69cd11e05bbee38de52431cff2b9
tommytim0515 <xtianae@connect.ust.hk>
2022-02-22 16:14:20 +0800

    add ProveMembership benchmark

--- accumulator/benchmark_test.go
+++ accumulator/benchmark_test.go
@@ -25,6 +25,28 @@ func BenchmarkAccAndProve(b *testing.B) {
 	}
 }
 
+func BenchmarkProveMembership(b *testing.B) {
+	setSize := 10000
+	set := GenBenchSet(setSize)
+	rep := GenRepersentatives(set, HashToPrimeFromSha256)
+	setup := *TrustedSetup()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ProveMembership(&setup.G, &setup.N, rep)
+	}
+}
+
+func BenchmarkProveMembershipIter(b *testing.B) {
+	setSize := 10000
+	set := GenBenchSet(setSize)
+	rep := GenRepersentatives(set, HashToPrimeFromSha256)
+	setup := *TrustedSetup()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ProveMembershipIter(&setup.G, &setup.N, rep)
+	}
+}
+
 func BenchmarkDIHash(b *testing.B) {
 	testBytes := []byte(testString)
 	b.ResetTimer()
