835959c0538a512e9bda04e47e5920a92c065ab2
jiajun <jiajun_xin@outlook.com>
2022-03-17 11:02:43 +0800

    fix test bug

--- main.go
+++ main.go
@@ -21,11 +21,11 @@ func GenSortedListSet(inputList []*big.Int) []*big.Int {
 	tempHashInput[1] = inputList[0]
 
 	poseidonHashResult[0], err = poseidon.Hash(tempHashInput)
-
 	if err != nil {
 		// not expecting error from a Hash function
 		panic(err)
 	}
+
 	for i := 1; i < setsize; i++ {
 		tempHashInput[0] = inputList[i-1]
 		tempHashInput[1] = inputList[i]
@@ -35,10 +35,10 @@ func GenSortedListSet(inputList []*big.Int) []*big.Int {
 			panic(err)
 		}
 	}
+
 	tempHashInput[0] = inputList[setsize-1]
 	tempHashInput[1] = max
 	poseidonHashResult[setsize], err = poseidon.Hash(tempHashInput)
-
 	if err != nil {
 		// not expecting error from a Hash function
 		panic(err)
@@ -46,9 +46,8 @@ func GenSortedListSet(inputList []*big.Int) []*big.Int {
 	return poseidonHashResult
 }
 
-// genDelListSet is simply used for testing.
-// We will generate a inputList which do not include min and max and it is a subset of SortedListSet
-func genDelListSet(inputList []*big.Int) []*big.Int {
+// genUpdateListSet is simply used for testing.
+func genUpdateListSet(inputList []*big.Int) []*big.Int {
 	var err error
 	setsize := len(inputList)
 
@@ -81,6 +80,7 @@ func main() {
 	setup := accumulator.TrustedSetup()
 	oldSetSize := 10000
 	delSetSize := 16
+	//	addSetSize := 32
 
 	// generate Accumulator_old
 	oldSortedList := make([]*big.Int, oldSetSize)
@@ -95,23 +95,23 @@ func main() {
 	AccOld := accumulator.AccumulateNew(setup.G, prodOldSet, setup.N)
 
 	fmt.Println("Accumulator_old = ", AccOld.String())
-	// Generate test set
-	// Note that sortedList1 do not have to be continuous, we use this set just for example.
+	// Generate delete set
+	// Note that delList do not have to be continuous, we use this set just for example.
 	delList := make([]*big.Int, delSetSize)
-	// i start from 1 to avoid 0 as input. 0 is already reserved for min value.
+	// delList start from 1 to avoid 0 as input. 0 is already reserved for min value.
 	for i := 0; i < delSetSize; i++ {
 		delList[i] = big.NewInt(int64(1 + i*2))
 	}
 	// delListSet should be a subset of oldSortedListSet
-	delListSet := genDelListSet(delList)
+	delListSet := genUpdateListSet(delList)
 	prodDelSet := CalSetProd(delListSet)
 	var prodMidSet big.Int
 	prodMidSet.Div(prodOldSet, prodDelSet)
 	AccMid := accumulator.AccumulateNew(setup.G, &prodMidSet, setup.N)
 	fmt.Println("Accumulator_mid = ", AccMid.String())
 
-	// AccTest := accumulator.AccumulateNew(&prodMidSet, prodDelSet, setup.N)
-	// fmt.Println("AccTest = ", AccTest.String()) //AccTest should be the same as the AccOld
+	AccTest := accumulator.AccumulateNew(AccMid, prodDelSet, setup.N)
+	fmt.Println("AccTest = ", AccTest.String()) //AccTest should be the same as the AccOld
 
 	l1 := accumulator.HashToPrime(append(AccOld.Bytes(), AccMid.Bytes()...))
 	fmt.Println("primeChallenge = ", l1.String())
@@ -121,12 +121,23 @@ func main() {
 	q1, r1 = q1.DivMod(prodDelSet, l1, r1)
 	Q1 := accumulator.AccumulateNew(AccMid, q1, setup.N)
 	fmt.Println("Q1 = ", Q1.String())
-	// fmt.Println("r1 = ", r1.String())
+	fmt.Println("r1 = ", r1.String())
 
 	AccTest1 := accumulator.AccumulateNew(Q1, l1, setup.N)
 	AccTest2 := accumulator.AccumulateNew(AccMid, r1, setup.N)
 	AccTest3 := AccTest1.Mul(AccTest1, AccTest2)
 	AccTest3.Mod(AccTest3, setup.N)
 	fmt.Println("Accumulator_test = ", AccTest3.String()) //AccTest3 should be the same as the AccOld
+
+	// Generate delete set
+	// Note that delList do not have to be continuous, we use this set just for example.
+	// addList := make([]*big.Int, addSetSize)
+	// // addList start from 1 to avoid 0 as input. 0 is already reserved for min value.
+	// for i := 0; i < addSetSize; i++ {
+	// 	addList[i] = big.NewInt(int64(1 + i))
+	// }
+	// // delList should be a subset of addList
+	// addListSet := genUpdateListSet(addList)
+	// prodAddSet := CalSetProd(addListSet)
 
 }
