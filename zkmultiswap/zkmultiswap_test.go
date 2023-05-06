753e8f50cfb6f4660a81e7eaa97ea1263def5ec0
Jiajun Xin <jiajun_xin@outlook.com>
2023-05-06 23:04:03 +0800

    add test for invalid cases

--- zkmultiswap/zkmultiswap_test.go
+++ zkmultiswap/zkmultiswap_test.go
@@ -50,3 +50,45 @@ func TestZkMultiSwap(t *testing.T) {
 	witness = *AssignCircuit(testSet)
 	assert.SolvingSucceeded(&circuit, &witness, test.WithCurves(ecc.BN254))
 }
+
+func TestZkMultiSwapFailCases(t *testing.T) {
+	assert := test.NewAssert(t)
+	var circuit, witness Circuit
+	testSetSize := uint32(10)
+
+	circuit = *InitCircuitWithSize(testSetSize)
+
+	testSet := GenTestSet(testSetSize, accumulator.TrustedSetup())
+	witness = *AssignCircuit(testSet)
+
+	// case for incorrect update of sum
+	witness.UpdatedBalances[0] = 5
+	assert.SolvingFailed(&circuit, &witness, test.WithCurves(ecc.BN254))
+	//-------------------
+	witness = *AssignCircuit(testSet)
+	witness.OriginalSum = 10
+	assert.SolvingFailed(&circuit, &witness, test.WithCurves(ecc.BN254))
+	//-------------------
+	witness = *AssignCircuit(testSet)
+	witness.UpdatedSum = 10
+	assert.SolvingFailed(&circuit, &witness, test.WithCurves(ecc.BN254))
+	//-------------------
+	witness = *AssignCircuit(testSet)
+	witness.OriginalBalances[0] = 10
+	assert.SolvingFailed(&circuit, &witness, test.WithCurves(ecc.BN254))
+
+	// case for incorrect range of user balance
+	witness = *AssignCircuit(testSet)
+	witness.OriginalBalances[0] = 1000000000
+	witness.UpdatedBalances[0] = 1000000000
+	assert.SolvingFailed(&circuit, &witness, test.WithCurves(ecc.BN254))
+
+	// case for incorrect remainders
+	witness = *AssignCircuit(testSet)
+	witness.RemainderR1 = testSet.ChallengeL1
+	assert.SolvingFailed(&circuit, &witness, test.WithCurves(ecc.BN254))
+	//-------------------
+	witness = *AssignCircuit(testSet)
+	witness.RemainderR2 = testSet.ChallengeL2
+	assert.SolvingFailed(&circuit, &witness, test.WithCurves(ecc.BN254))
+}
