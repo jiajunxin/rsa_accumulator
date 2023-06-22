ff4df54fc7dee464324fa47097fab0ca7661c969
Jiajun Xin <jiajun_xin@outlook.com>
2023-06-22 17:38:43 +0800

    remove redundant code

--- zkmultiswap/zkmultiswap_test.go
+++ zkmultiswap/zkmultiswap_test.go
@@ -1,9 +1,7 @@
 package zkmultiswap
 
 import (
-	"bytes"
 	"fmt"
-	"math/big"
 	"reflect"
 	"testing"
 
@@ -12,32 +10,6 @@ import (
 	"github.com/consensys/gnark/test"
 	"github.com/jiajunxin/rsa_accumulator/accumulator"
 )
-
-// compare return 0 if input1 == input2
-func compare(input1 *big.Int, input2 []byte) int {
-	input1bytes := input1.Bytes()
-	return bytes.Compare(input1bytes, input2)
-}
-
-// func TestPoseidonHash(t *testing.T) {
-// 	inputs := "anything"
-// 	result1, err := iden3Poseidon.HashBytes([]byte(inputs))
-// 	if err != nil {
-// 		t.Errorf(err.Error())
-// 	}
-
-// 	poseidonHasher := bnPoseidon.NewPoseidon()
-// 	_, err = poseidonHasher.Write([]byte(inputs))
-// 	if err != nil {
-// 		t.Errorf(err.Error())
-// 	}
-// 	result2 := poseidonHasher.Sum(nil)
-
-// 	if compare(result1, result2) != 0 {
-// 		fmt.Println("result1 = ", result1.String())
-// 		fmt.Println("result2 = ", result2)
-// 	}
-// }
 
 func TestPublicWitness(t *testing.T) {
 	testSetSize := uint32(10)
